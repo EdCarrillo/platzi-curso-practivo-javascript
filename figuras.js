bc25035cf473e8b979ca50d9cb5a61616ecc3547
Eduard Carrillo <Edcarrillo29@gmail.com>
2022-02-22 11:10:46 -0400

    funciones de figuras geometricas

---
+++ figuras.js
@@ -0,0 +1,55 @@
+// Código del cuadrado
+
+
+function perimetroCuadrado (lado){
+    return lado * 4;
+}
+
+perimetroCuadrado()
+
+
+function areaCuadrado(lado){
+    return lado * lado
+}
+
+areaCuadrado()
+// Código del cuadrado
+
+
+
+// Código del Trángulo
+
+
+function perimetroTriangulo(lado1, lado2, base){
+return lado1 + lado2 + base
+}
+
+function areaTriangulo(base, altura){
+    return (base*altura) /2;
+}
+
+// Código del Trángulo
+
+
+
+// Código del círculo
+
+function diametroCirculo(radio){
+return radio *2
+}
+
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio);
+    return diametro * Math.PI;
+}
+
+function areaCirculo(radio){
+    return (radio * radio) * Math.PI;
+}
+
+// Código del círculo
+
+
+
+
+
